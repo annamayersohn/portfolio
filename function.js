eaa1a4bdd1ddd3bbec2e96c514bd5d34c827e1e1
annamayersohn <78816158+annamayersohn@users.noreply.github.com>
2021-12-04 19:16:45 -0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,41 @@
+
+function menu() {
+    var x=document.querySelector("#nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
+
+
+$(document).ready(function(){
+    $(".source").hover(function(){
+        $(".source").scrollTop(0);
+        }, function(){
+        $(".source").scrollTop(0);
+    });
+});
+
+$(document).ready(function(){
+    $(".source").focus(function(){
+        $(".source").scrollTop(0);
+        }, function(){
+        $(".source").scrollTop(0);
+    });
+});
+
+
+var mw = document.querySelector(".modal-word");
+// var mc = document.querySelector(".modal-container")
+// Double condition: if (con1 && con2)
+
+if (mw) {
+    mw.addEventListener("click", function() {
+        document.querySelector(".modal-container").style.display = "block"
+    });
+    
+    document.querySelector(".close").addEventListener("click", function() {
+        document.querySelector(".modal-container").style.display = "none"
+    });
+}
